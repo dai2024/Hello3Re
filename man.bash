7f65d29cdfb1b1cee1309dccb030c11c99b5e9d7
dai2023 <83387146+dai2024@users.noreply.github.com>
2023-01-06 01:59:23 +0000

    Update man.bash
    e

--- man.bash
+++ man.bash
@@ -4,5 +4,18 @@
 # man -k who
 # man -k emacs
 # man -k mule
-man -k set
+# man -k set
+set -e
+touch readme.md
+ls -al
+chmod 666 readme.md
+ls -al
+chmod 644 readme.md
+ls -al
+chmod go+w readme.md
+ls -al
+chmod go-w readme.md
+ls -al
+chmod -g-o readme.md
+ls -al
 echo Hello, world!

1d5d5846a54e54ff4ff6e3a67b73cd19c3945342
dai2023 <83387146+dai2024@users.noreply.github.com>
2023-01-15 06:51:29 +0000

    Create man2.bash
    new

---
+++ man2.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set | grep history
+mkdir -m 700 test700
+cd test700
+ls -al
+touch -t 202401010000 readme.md
+chmod 666 readme.md
+ls -al
+echo "!" >> readme.md
+cat readme.md
+od -t c readme.md
+echo "Hello, world!"

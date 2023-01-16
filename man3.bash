ad98d416376f5e8a8d9157f83a4b8a13da4ed99c
dai2023 <83387146+dai2024@users.noreply.github.com>
2023-01-16 10:53:18 +0000

    Create man3.bash
    pat1

---
+++ man3.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set | grep history
+mkdir -m 700 test700 && cd test700
+echo -n '!"#$%&'"'"'()' | xxd > hello3.dump
+ls hello3.dump && cat hello3.dump
+sed 's/!/~/' hello3.dump > hello3.c
+ls hello3.c && diff hello3.c hello3.dump
+ls hello3.c && xxd -i hello3.c
+ls hello3.dump && xxd -r hello3.dump > hello3.txt
+ls hello3.txt && cat hello3.txt

018c318880f330895cb4a1437d870a544918a323
dai <83387146+dai2024@users.noreply.github.com>
2023-03-08 10:23:53 +0000

    Update man1.bash

--- man1.bash
+++ man1.bash
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-set -e
-
-if [ ! -d "test700" ]; then
-  mkdir test700
-fi
-chmod 700 test700
+set | grep history
+mkdir -m 700 test700
 cd test700
-
-filename=$(mktemp)
-touch "$filename"
-chmod 600 "$filename"
-echo "0123456789ABCDEF" >> "$filename"
-cat "$filename"
-od -t c "$filename"
+ls -al
+touch readme.md
+chmod 644 readme.md
+ls -al
+echo "0123456789ABCDEF" >> readme.md
+cat readme.md
+od -t c readme.md
+echo Hello, world!

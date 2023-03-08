e6484b2fdd9cf050208b6dac904e51b27caa475b
dai <83387146+dai2024@users.noreply.github.com>
2023-03-08 09:34:13 +0000

    Update man.bash
    The command vi -c q ./readme.md opens the readme.md file and prompts the user to exit the editor. However, this command may save the file even if the user interrupts the editing process. This poses a security threat for files that may contain sensitive information. It is recommended to remove this line.

--- man.bash
+++ man.bash
@@ -1,29 +1,13 @@
 #!/usr/bin/env bash
-# echo -e -> set -e
-# man -k man
-# man -k who
-# man -k emacs
-# man -k mule
-# man -k set
-# set -e
-set | grep history
-mkdir -m 700 test700
+mkdir -p -m 700 test700
 cd test700
 ls -al
 touch readme.md
 ls -al
-chmod 666 readme.md
+chmod 600 readme.md
 ls -al
-chmod 644 readme.md
+echo "This is a test file." > readme.md
 ls -al
-chmod go+w readme.md
+chmod 400 readme.md
 ls -al
-chmod go-w readme.md
-ls -al
-chmod 666 readme.md
-ls -al
-chmod -g-o readme.md
-ls -al
-vi -c q ./readme.md
-ls -al
-echo Hello, world!
+echo "Hello, world!"

98d07042f2b385987ef31badd158555966cde4b3
dai <83387146+dai2024@users.noreply.github.com>
2023-03-08 10:22:00 +0000

    Update man.bash

--- man.bash
+++ man.bash
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
-mkdir -p -m 700 test700
+# echo -e -> set -e
+# man -k man
+# man -k who
+# man -k emacs
+# man -k mule
+# man -k set
+# set -e
+set | grep history
+mkdir -m 700 test700
 cd test700
 ls -al
 touch readme.md
 ls -al
-chmod 600 readme.md
+chmod 666 readme.md
 ls -al
-echo "This is a test file." > readme.md
+chmod 644 readme.md
 ls -al
-chmod 400 readme.md
+chmod go+w readme.md
 ls -al
-echo "Hello, world!"
+chmod go-w readme.md
+ls -al
+chmod 666 readme.md
+ls -al
+chmod -g-o readme.md
+ls -al
+vi -c q ./readme.md
+ls -al
+echo Hello, world!
